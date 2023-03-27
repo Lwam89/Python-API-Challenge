0c610de457dbc45f1aab5b25c528c06579c48b16
Lwam89 <120775853+Lwam89@users.noreply.github.com>
2023-03-26 20:25:49 -0700

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key =  "53af080382f0b68e69df4bdf38e98cac"
+weather_api_key =  "key here"
 
 # Geoapify API 
-g_key = "AIzaSyAfzeIlKWsOgX5yTLbRGMNm05diRAPRT_Q"
+g_key = "key here"
 
